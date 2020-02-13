76088a9c9e742bff23a2ffe44a85fe613676f9fc
sergey-ozernyy <50018166+sergey-ozernyy@users.noreply.github.com>
2020-02-13 07:57:54 +0300

    Update Game.kt

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,32 @@
-fun main() {
+    class Player(_name:String, val health:Int){
+        val race = "DWARF"
+        var town = "Bavaria"
+        val name = _name
+        val aligment: String
+        private var age = 0
+        
+        init {
+            println("initializing player")
+            aligment = "GOOD"
+        }
+        
+        constructor(_name:String): this(_name, 100) {
+            town = "The Shire"
+        }
+        
+    }
     
-}
+    
+    fun main(args: Arrey<String>){
+        Player("Madrigal")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
