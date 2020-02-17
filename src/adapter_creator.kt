4bd155e221daf2d5b1adf7b8ef756103f8b07cce
sergey-ozernyy <50018166+sergey-ozernyy@users.noreply.github.com>
2020-02-17 14:52:15 +0300

    Create adapter_creator.kt

---
+++ src/adapter_creator.kt
@@ -0,0 +1,19 @@
+var adapter = ArrayAdapter<String>(
+  this,
+  android.R.layout.simple_list_item1,
+  arrayOf<String>("Рыжик", "Барсик", "Мурзик"))
+-----
+val catNames = arrayOf<String>("Рыжик", "Барсик", "Мурзик")
+val adapter = ArrayAdapter<String>(this, 
+                                   R.layout.list_item, 
+                                   catNames)
+listView.setAdapter(adapter)  
+--------------
+val spinner = findViewById(R.id.spinner1) as Spinner
+val adapter = ArrayAdapter.createFromResource(
+  this, R.array.dayofweek, android.R.layout.simple_spinner_item)
+adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+spinner.setAdapter(adapter)
+  
+  
+  
